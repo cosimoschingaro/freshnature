9b99c41059f5e3f9e33fb0ca2db4a56af0c89114
Cosimo Schingaro <cosimo.schingaro@hotmail.com>
2021-06-06 17:52:33 +0200

    aggiustare footer per sm

--- code.js
+++ code.js
@@ -5,18 +5,20 @@ const menuOpener = document.querySelector('#menu-opener')
         })
 
         // FOOTER BUTTON
+        
         let opener = document.querySelector('.opener')
              let circles = document.querySelectorAll('.circle')
              let opened = false
              opener.addEventListener('click', ()=>{
-                 if(opened == false){
+                 
+                if(opened == false){
                      circles.forEach((circle, i) =>{
-                     circle.style.transform = `translate(${i*100}px) translateX(100px) `
+                     circle.style.transform = `rotate(-${i*360/circles.length}deg) translateX(80px) rotate(${i*360/circles.length}deg)`
                  })
                      opened = !opened
                  }else {
                      circles.forEach((circle, i) =>{
-                     circle.style.transform = ` translateX(0px) `
+                     circle.style.transform = `rotate(-${i*360/circles.length}deg) translateX(0px) rotate(${i*360/circles.length}deg)`
                  })
                     
                  }    
